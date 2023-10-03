066658fdcb2c4fd840c976082d12acdfeacedb2a
Niketan <niketanb1234@gmail.com>
2023-10-03 19:22:42 +0530

    first commit

--- mainbackend.js
+++ mainbackend.js
@@ -7,10 +7,10 @@ app.use(express.json());
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-    host: "sql9.freesqldatabase.com",
-  user: "sql9648816",
-  password: "KGMFX14DH5",
-  database:"sql9648816",
+  host: "sql9.freesqldatabase.com",
+  user: "sql9650544",
+  password: "WFPxEPww7g",
+  database:"sql9650544",
   port:'3306'
 });
 
@@ -24,7 +24,7 @@ app.post('/tpdata',async (req,res)=>{
     });
 })
 app.post('/tp',async (req,res)=>{
-    
+    console.log("Hii")
     
     var tab ="SELECT * from TpData";
     con.query(tab, function (err, result) {
